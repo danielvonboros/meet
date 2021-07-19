7a5fe14620defe591b34e94472b236b4cc421d03
danielvonboros <dboros40@gmail.com>
2021-07-19 23:14:08 +0200

    update CitySearch component
    src/CitySearch.js
    - update state when the input changes

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -6,10 +6,20 @@ class CitySearch extends Component {
     query: "",
   };
 
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    this.setState({ query: value });
+  };
+
   render() {
     return (
       <div className="CitySearch">
-        <input type="text" className="city" value={this.state.query} />
+        <input
+          type="text"
+          className="city"
+          value={this.state.query}
+          onChange={this.handleInputChanged}
+        />
         <ul className="suggestions"></ul>
       </div>
     );
