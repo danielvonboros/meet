581c11d6745cc56ad8b431c2fc5412af4cd84303
danielvonboros <dboros40@gmail.com>
2021-07-20 09:28:43 +0200

    replaced placeholder array with mockData in EventList
    replaced placeholder array with MockData in EventList

--- src/tests/EventList.test.js
+++ src/tests/EventList.test.js
@@ -2,10 +2,11 @@ import React from "react";
 import { shallow } from "enzyme";
 import EventList from "../EventList";
 import Event from "../Event";
+import { mockData } from "../mock-data";
 
 describe("<EventList /> component", () => {
   test("render correct number of events", () => {
-    const EventListWrapper = shallow(<EventList events={[{}, {}, {}, {}]} />);
-    expect(EventListWrapper.find(Event)).toHaveLength(4);
+    const EventListWrapper = shallow(<EventList events={mockData} />);
+    expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
   });
 });
