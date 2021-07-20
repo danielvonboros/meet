c2a52e8e32b9f1cc0d7cff061f43ae45bfb78a17
danielvonboros <dboros40@gmail.com>
2021-07-20 09:25:11 +0200

    removed unused vars/components from CitySearch component

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import App from "./App";
 
 class CitySearch extends Component {
   state = {
--- src/tests/CitySearch.test.js
+++ src/tests/CitySearch.test.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { shallow } from "enzyme";
 import CitySearch from "../CitySearch";
 import { extractLocations } from "../api";
