ef78a78fe7be9f3c8d957122731084f6e8baf94c
danielvonboros <dboros40@gmail.com>
2021-07-20 09:22:42 +0200

    add click on suggestion CitySearch/CitySearch.test
    src/CitySearch.js
    - add click on suggestion

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -15,6 +15,10 @@ class CitySearch extends Component {
     this.setState({ query: value, suggestions });
   };
 
+  handleItemClicked = (suggestion) => {
+    this.setState({ query: suggestion });
+  };
+
   render() {
     return (
       <div className="CitySearch">
@@ -26,7 +30,12 @@ class CitySearch extends Component {
         />
         <ul className="suggestions">
           {this.state.suggestions.map((suggestion) => (
-            <li key={suggestion}>{suggestion}</li>
+            <li
+              key={suggestion}
+              onClick={() => this.handleItemClicked(suggestion)}
+            >
+              {suggestion}
+            </li>
           ))}
           <li key="all">
             <b>See all cities</b>
