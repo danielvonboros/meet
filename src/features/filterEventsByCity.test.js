6e50e02631dab856301c01df1551af6fed3db225
danielvonboros <dboros40@gmail.com>
2021-08-04 19:28:17 +0200

    update FilterEventsByCity.test.js
    expected output: 2
    received output: 0

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -39,24 +39,30 @@ defineFeature(feature, test => {
     });
 
     test('User can select a city from the suggested list', ({ given, and, when, then }) => {
-        given('user was typing “Berlin” in the city textbox', () => {
-
-        });
+        let AppWrapper;
+        given('user was typing “Berlin” in the city textbox', async () => {
+      AppWrapper = await mount(<App />);
+      AppWrapper.find('.city').simulate('change', { target: { value: 'Berlin' } });
+    });
 
         and('the list of suggested cities is showing', () => {
-
+                AppWrapper.update();
+                expect(AppWrapper.find('.suggestions li')).toHaveLength(2);
         });
 
         when('the user selects a city (e.g., “Berlin, Germany”) from the list', () => {
-
+                AppWrapper.find('.suggestions li').at(0).simulate('click');
+              
         });
 
         then('their city should be changed to that city (i.e., “Berlin, Germany”)', () => {
-
-        });
+            const CitySearchWrapper = AppWrapper.find(CitySearch);
+        expect(CitySearchWrapper.state('query')).toBe('Berlin, Germany');
+         });
+        
 
         and('the user should receive a list of upcoming events in that city', () => {
-
+            expect(AppWrapper.find('.event')).toHaveLength(mockData.length);
         });
     });
 
