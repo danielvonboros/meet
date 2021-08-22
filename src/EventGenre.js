bbf9d0027abd52ec86b78be204110382cbb4f056
danielvonboros <dboros40@gmail.com>
2021-08-22 22:55:42 +0200

    code correction label of PieChart

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -36,7 +36,9 @@ const EventGenre = ({ events }) => {
           fill="#8884d8"
           paddingAngle={5}
           dataKey="value"
-          label
+          label={({ name, percent }) =>
+            `${name} ${(percent * 100).toFixed(0)}%`
+          }
         >
           {data.map((entry, index) => (
             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
